35fa8bdebcf49105b31e5486992f0eab7f920ccf
Nik <niklauskun@gmail.com>
2020-07-16 13:42:56 -0400

    Update case_creation_main.py

--- case_creation_main.py
+++ case_creation_main.py
@@ -19,7 +19,7 @@ from case_creation_functions import write_RTS_case
 
 # create a file structure object, "f"
 folder_path = os.path.join(os.environ["HOMEPATH"], "Desktop")
-MPEC_folder_path = "test715"
+MPEC_folder_path = "competitiveMPEC"
 f = DirStructure(
     folder_path, RTS_folder="RTS-GMLC-master", MPEC_folder=MPEC_folder_path,
 )  # the first arg should be the local directory you put NREL-RTS in
@@ -32,9 +32,9 @@ f.make_directories()
 data_class = LoadNRELData(f)
 kw_dict = data_class.load_nrel_data()
 kw_dict = data_class.define_constants(kw_dict)
-# data_class.add_unit(
-#    [301], ["313_STORAGE_1"]
-# )  # [323, 301, 301], ["322_HYDRO_1", "303_WIND_1", "313_STORAGE_1"]
+data_class.add_unit(
+    [301], ["313_STORAGE_1"]
+)  # [323, 301, 301], ["322_HYDRO_1", "303_WIND_1", "313_STORAGE_1"]
 
 # inputs for running
 start = datetime.datetime.strptime("01-01-2019", "%m-%d-%Y")  # day case starts on
